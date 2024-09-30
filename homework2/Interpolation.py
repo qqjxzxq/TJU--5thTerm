c76c3438c79772603b5d90cbfa2c9e71fc4c76ac
qqjxzxq <1376782660@qq.com>
2024-09-30 20:09:16 +0800

    范德蒙德算误差写完了

--- homework2/Interpolation.py
+++ homework2/Interpolation.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from sympy import symbols, sin, cos, lambdify
+import random
 
 plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
 
@@ -11,41 +12,58 @@ class Interpolation:
         self.end = end
         self.num_points = num_points
         self.func_params = func_params
+        self.points = None
         self.exp_points = exp_points
         self.pi = np.pi
 
         self.t = symbols('t')  # 定义符号变量
-        self.x = self.chebyshev_nodes()
-        self.y = self.compute_function_values()
+        self.x, self.y = self.UniformSampling() # 1 使用均匀采样
+        #self.x, self.y = self.chebyshev_sampling() # 2 使用切比雪夫采样
+        self.Vandermonde = self.VandermondeError() # 计算范德蒙德多项式插值及其误差
         
+    def func(self, x):
+        a, b, c, d = self.func_params 
+        fx = int(a) * np.sin(int(b) * x) + int(c) * np.cos(int(d) * x) 
+        return fx
+        
+    #使用均匀采样n+1个点
+    def UniformSampling(self):
+        
+        self.x = np.linspace(self.start, self.end, self.num_points)
+        self.y = self.func(self.x)
+        
+        self.points = list(zip(self.x, self.y))
+        for point in self.points:
+            print(f"点: x = {point[0]}, y = {point[1]}")
+        
+        return self.x, self.y
+    
     
     #使用chebyshev采样n+1个点    
-    def chebyshev_nodes(self):
+    def chebyshev_sampling(self):
         nodes = []
         for i in range(self.num_points):
             x_i = 0.5 * (self.start + self.end) + 0.5 * (self.end - self.start) * np.cos((2 * i + 1) / (2 * self.num_points) * np.pi)
             nodes.append(x_i)
-        return np.array(nodes)
-
-    def compute_function_values(self):
-        a, b, c, d = self.func_params
-        func = lambdify(self.t, int(a) * sin(int(b) * self.t) + int(c) * cos(int(d) * self.t))
-        return func(self.x)
-
-    def display_points(self):
-        points = list(zip(self.x, self.y))
-        # for point in points:
-        #     print(f"点: x = {point[0]}, y = {point[1]}")
-            
     
+        self.x = np.array(nodes)
+        self.y = func(self.x)
+    
+        self.points = list(zip(self.x, self.y))
+        for point in self.points:
+            print(f"点: x = {point[0]}, y = {point[1]}")
+            
+        return self.x, self.y
+
+        
 #Vandermonde
-    def Vandermonde(self):
+    def Vandermonde(self, x):
     # 初始化 Vandermonde 矩阵
         X = np.zeros((self.num_points, self.num_points))
         for i in range(self.num_points):
-                for j in range(self.num_points):
-                    X[i][j] = np.pow(self.x[i], j)
-    
+            for j in range(self.num_points):
+                X[i][j] = np.power(self.x[i], j)
+
     # 构造系数矩阵 Y
         Y = np.array(self.y).reshape(-1, 1)  # 转换为列向量
 
@@ -53,13 +71,29 @@ class Interpolation:
         A = np.linalg.solve(X, Y)
 
     # 计算多项式值 P
-        def polynomial(x):
-            P = 0
-            for i in range(self.num_points):
-                P += A[i][0] * np.power(x, i)
-            return P
+        P = 0
+        
+        for i in range(self.num_points):
+            P += A[i][0] * np.power(x, i)
+            print("插值结果：x = ", self.x[i] , ", y = ", P)
+            
+        return P
+    
 
-        return polynomial
+    def VandermondeError(self):
+        #随机选取m个点
+        N = range(self.start, self.end)
+        node = random.sample(N, self.exp_points)
+        Rn = []
+        for i in range(self.exp_points):  
+            error = self.func(node[i]) - self.Vandermonde(node[i])
+            Rn.append(error)
+            print("x = " , node[i] , "误差R = " , Rn[i])
+        
+        mean = np.mean(Rn)
+        print("范德蒙德多项式插值平均误差：", mean)
+            
+    
 
 
 #Lagrange
@@ -89,8 +123,7 @@ m = int(input("输入实验点个数 m ："))
 # 构造插值对象
 interp = Interpolation(start=int(a), end=int(b), num_points=n + 1, func_params=(c, d, e, f), exp_points= m)
 
-# 显示生成的点
-interp.display_points()
+
 
 
 
