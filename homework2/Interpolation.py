1a879b71db3a564b130dc8cfc564fcb40167525d
qqjxzxq <1376782660@qq.com>
2024-10-04 00:19:08 +0800

    能画图但是好像算不对

--- homework2/Interpolation.py
+++ homework2/Interpolation.py
@@ -19,7 +19,8 @@ class Interpolation:
         self.t = symbols('t')  # 定义符号变量
         self.x, self.y = self.UniformSampling() # 1 使用均匀采样
         #self.x, self.y = self.chebyshev_sampling() # 2 使用切比雪夫采样
-        self.Vandermonde = self.VandermondeError() # 计算范德蒙德多项式插值及其误差
+        self.VandermondeResult = self.VandermondeError() # 计算范德蒙德多项式插值及其误差
+        self.plot = self.plot_interpolation_vs_target() # 画图
         
     def func(self, x):
         a, b, c, d = self.func_params 
@@ -113,6 +114,39 @@ class Interpolation:
 #分段三次Hermite插值
 
 
+
+
+    def plot_interpolation_vs_target(self):
+    # 选择 m 个实验点
+        
+        exp_x = np.linspace(self.start, self.end, self.exp_points)
+        
+        exp_y_target = np.zeros(self.exp_points)
+        exp_y_interp1 = np.zeros(self.exp_points)
+        i = 0
+        for i in range(exp_x.size):
+            exp_y_target[i] = self.func(exp_x[i])  # 目标函数值
+            exp_y_interp1[i] = self.Vandermonde(exp_x[i])  # 插值函数值
+    
+    # 绘制目标函数和插值函数
+        plt.figure(figsize=(10, 6))
+        plt.plot(exp_x, exp_y_target, label='目标函数', color='blue')
+        plt.plot(exp_x, exp_y_interp1, label='插值函数', color='red', linestyle='--')
+    
+    # 绘制误差
+        error = exp_y_target - exp_y_interp1
+        plt.figure(figsize=(10, 6))
+        plt.plot(exp_x, error, label='误差', color='green')
+    
+    # 图例和标题
+        plt.legend()
+        plt.title('插值函数与目标函数对比')
+        plt.xlabel('x')
+        plt.ylabel('函数值')
+        plt.grid()
+        plt.show()
+
+
 ############# 程序入口 ############
 
 a, b = input("输入插值区间：").split()
@@ -122,7 +156,6 @@ m = int(input("输入实验点个数 m ："))
 
 # 构造插值对象
 interp = Interpolation(start=int(a), end=int(b), num_points=n + 1, func_params=(c, d, e, f), exp_points= m)
-
 
 
 
